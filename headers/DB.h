8ec93c6f391abda687f5293a49897866de9ea0be
Camboy <camille.briand.20160281@efrei.net>
2017-05-01 13:45:46 +0200

    Beginning the book structure

---
+++ headers/DB.h
@@ -0,0 +1,15 @@
+#ifndef DEF_DB
+#define DEF_DB
+
+  #include <stdio.h>
+  #include <stdlib.h>
+
+  tydef struct Book
+  {
+    char[50] title;
+    char[50] author;
+    char[8] code;
+    int inStock;
+  } Book;
+
+#endif
