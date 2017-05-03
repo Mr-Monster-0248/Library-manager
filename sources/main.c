1c460dff06f23e81f00ae74a4c08642daf0549e4
Camboy <camille.briand.20160281@efrei.net>
2017-05-03 15:01:23 +0200

    Started to use curses.h functions

--- sources/main.c
+++ sources/main.c
@@ -1,7 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <curses.h>
+
+void curses_init();
+void curses_stop();
+
+
 
 int main(int argc, char** argv)
 {
+  curses_init();
+
   return EXIT_SUCCESS;
+}
+
+
+void curses_init()
+{
+  //Init curses
+  initscr();
+
+  //Deactivation of buffer at TTY pilot
+  cbreak();
+
+  //Deactivation of automatic display of entries
+  noecho();
+
+  //Allow keyboard capture (including arrow keys)
+  keypad(stdscr, TRUE);
+}
+
+void curses_stop()
+{
+  endwin();
 }
